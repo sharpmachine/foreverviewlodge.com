8a85616944ec5754e66594d07f7c000985ba7dcd
sharpmachine <jessekade@gmail.com>
2012-07-26 13:17:27 -0700

    trying out the homepage

--- wp-content/themes/my-mobile-page-v3/mobile-home.php
+++ wp-content/themes/my-mobile-page-v3/mobile-home.php
@@ -6,56 +6,73 @@
  */
 
 get_header(); ?>
-<div id="pagecontainer">
-        
-      <?php wp_reset_query(); ?>
-   	 
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
-		<?php if(!is_front_page()): ?>
-			<h1><?php the_title(); ?></h1>
-		<?php endif; ?>
-        
-			<div class="entry">
-				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-			</div>
+<div id="container">
 
-		<?php endwhile; endif; ?> 
-        
-	 <?php if($theme->get_option('show_widget_area_2') == 'enable') {
-     if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Archive widget area') ) : ?>
-     <h2>You can add widgets here from Admin->Appearance->Widgets <br /> Enable or Disable this widget area from Theme Options -> Widgets</h2>
-    <?php endif; } else {}?>  
-        
-      <div class="clear"></div>  
-      
-		<div class="icons_nav home_nav">
-      <?php if($theme->get_option('icons_arrows') == 'enable') { ?> <div class="paginated"> <?php } ?> 
-          <ul class="slides">
-				<?php
-				$count = 1;
-				query_posts(array( 'post_type' => 'icons_menu', 'orderby' => 'menu_order', 'order' => 'ASC', 'showposts' => '999')); ?>
-              <?php $postsnr = $wp_query->found_posts; ?>
-              <?php if (have_posts()) : ?>
-              <li>
-              <?php while (have_posts()) : the_post(); ?>
-              <a href="<?php echo get_post_meta($post->ID, "menu_item_url", $single = true); ?>" class="icon"><?php the_post_thumbnail('menu-icon-size'); ?><span><?php the_title(); ?></span></a>
-              <?php if ($postsnr > 4 && $count == 4){ ?>
-              </li><li>
-              <?php } if ($postsnr > 8 && $count == 8){ ?>
-              </li><li>
-              <?php } ?>
-              <?php $count++; endwhile; ?>
-				</li>
-              <?php if ($postsnr < 4 || $postsnr == 4){ ?>
-              <li></li>
-              <?php } ?>
-				<?php endif; ?>
-          </ul>
-       <?php if($theme->get_option('icons_arrows') == 'enable') { ?> </div> <?php } ?>  
+
+	<?php if ($theme->display('logo')) { ?> 
+        <div class="logo-image"><img src="<?php $theme->option('logo'); ?>" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>" /></div>
+    <?php } elseif ($theme->display('maintitle')) { ?> 
+        <div class="logo-title"><?php $theme->option('maintitle'); ?></div>
+    <?php } ?> 
+
+    <div id="main_panels">
+        <div class="panels_slider">
+        <ul class="slides">
+			<?php query_posts(array( 'post_type' => 'slider', 'showposts' => '9999'));?>
+            <?php if (have_posts()) : ?>
+            
+
+                    <?php while (have_posts()) : the_post(); ?>
+    
+                    <?php if (has_post_thumbnail( $post->ID ) ): ?>
+                    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+                    <li>
+                    <a href="<?php echo get_post_meta($post->ID, "slider_item_url", $single = true); ?>" class="icon">
+                    <img src="<?php echo $image[0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" />
+                    </a>
+                    </li>
+                    <?php endif; ?>
+                          
+                    <?php endwhile; ?>                          
+
+            <?php endif; ?>
+        </ul>
+        </div>
     </div>
-<?php get_footer(); ?>  
 
+    <div id="bottom_nav">
+        <div class="icons_nav">
+           <?php if($theme->get_option('icons_arrows') == 'enable') { ?> <div class="paginated"> <?php } ?> 
+            <ul class="slides">
+                
+					<?php
+					$count = 1;
+					query_posts(array( 'post_type' => 'icons_menu', 'orderby' => 'menu_order', 'order' => 'ASC', 'showposts' => '999')); ?>
+                    <?php $postsnr = $wp_query->found_posts; ?>
+                    <?php if (have_posts()) : ?>
+                    <li>
+                    <?php while (have_posts()) : the_post(); ?>
+                    <a href="<?php echo get_post_meta($post->ID, "menu_item_url", $single = true); ?>"><?php the_post_thumbnail('menu-icon-size'); ?><span><?php the_title(); ?></span></a>
+                    <?php if ($postsnr > 4 && $count == 4){ ?>
+                    </li><li>
+                    <?php } if ($postsnr > 8 && $count == 8){ ?>
+                    </li><li>
+                    <?php } ?>
+                    <?php $count++; endwhile; ?>
+					</li>
+                    <?php if ($postsnr < 4 || $postsnr == 4){ ?>
+                    <li></li>
+                    <?php } ?>
+					<?php endif; ?>
+                
+            </ul>
+          <?php if($theme->get_option('icons_arrows') == 'enable') { ?> </div> <?php } ?>  
+        </div>
+    </div>
+    
 </div>
+<?php $theme->option('analytics_code'); ?>
+</body>
+</html>
 
 
